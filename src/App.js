c32bb418243f327d67285047a64ab0e7da853e70
Julian Klett <julian22a@googlemail.com>
2023-11-13 15:14:29 +0100

    change app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Greeting name="Julian" age={27} />;
+}
+
+function Greeting({ name, age }) {
+  return (
+    <h1>
+      Hi my name is {name} and i´m {age} years old.
+    </h1>
+  );
 }
